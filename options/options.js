71477014be721ac1fdf819a246b6f3f4166fbbd7
Ming Yin <minyin1122@gmail.com>
2023-03-22 16:31:45 +0800

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "big-creative-expert-39498",
   url: "https://big-creative-expert-39498.botics.co",
   api: "https://big-creative-expert-39498.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
